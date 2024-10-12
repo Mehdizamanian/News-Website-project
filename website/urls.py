5a104ded5eb88617a9b45a911991abcbd13d9e50
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-10-12 09:40:26 +0330

    base , index , contact template fixec

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,11 @@ website urls
 """
 
 from django.urls import path 
-from .views import index
+from .views import index,contact
+
+app_name="website"
 
 urlpatterns = [
     path('', index,name="index"),
+    path('contact/', contact,name="contact"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 def index(request):
   return render(request,'website/tech-index.html')
 
+
+def contact(request):
+  return render(request,'website/tech-contact.html')
+
 # Create your views here.
